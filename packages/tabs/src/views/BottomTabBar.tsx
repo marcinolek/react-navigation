ecd17a8f603fb6f1cd6a0f9820f3f469bf46dc99
Marcin Olek <marcin@spotio.com>
2021-09-23 20:41:41 +0200

    fix(tabs): replace depraceted removeListener method with Emmiter remove, BottomBar

--- packages/tabs/src/views/BottomTabBar.tsx
+++ packages/tabs/src/views/BottomTabBar.tsx
@@ -123,23 +123,38 @@ class TabBarBottom extends React.Component<BottomTabBarProps, State> {
 
   componentDidMount() {
     if (Platform.OS === 'ios') {
-      Keyboard.addListener('keyboardWillShow', this._handleKeyboardShow);
-      Keyboard.addListener('keyboardWillHide', this._handleKeyboardHide);
+      this.showListener = Keyboard.addListener(
+        'keyboardWillShow',
+        this._handleKeyboardShow
+      );
+      this.hideListener = Keyboard.addListener(
+        'keyboardWillHide',
+        this._handleKeyboardHide
+      );
     } else {
-      Keyboard.addListener('keyboardDidShow', this._handleKeyboardShow);
-      Keyboard.addListener('keyboardDidHide', this._handleKeyboardHide);
+      this.showListener = Keyboard.addListener(
+        'keyboardDidShow',
+        this._handleKeyboardShow
+      );
+      this.hideListener = Keyboard.addListener(
+        'keyboardDidHide',
+        this._handleKeyboardHide
+      );
     }
   }
 
   componentWillUnmount() {
     if (Platform.OS === 'ios') {
-      Keyboard.removeListener('keyboardWillShow', this._handleKeyboardShow);
-      Keyboard.removeListener('keyboardWillHide', this._handleKeyboardHide);
+      if (this.showListener) this.showListener.remove();
+      if (this.hideListener) this.hideListener.remove();
     } else {
-      Keyboard.removeListener('keyboardDidShow', this._handleKeyboardShow);
-      Keyboard.removeListener('keyboardDidHide', this._handleKeyboardHide);
+      if (this.showListener) this.showListener.remove();
+      if (this.hideListener) this.hideListener.remove();
     }
   }
+
+  showListener!: any;
+  hideListener!: any;
 
   // @ts-ignore
   context: 'light' | 'dark';
